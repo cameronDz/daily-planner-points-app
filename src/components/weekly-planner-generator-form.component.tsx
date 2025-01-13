bfd8bbc67bc3a0a72cf39fe9851072ce433ae896
Cam Dziurgot <camerondziurgot@gmail.com>
2025-01-13 04:11:07 -0500

    new weekly tasks

--- src/components/weekly-planner-generator-form.component.tsx
+++ src/components/weekly-planner-generator-form.component.tsx
@@ -4,7 +4,7 @@ import { v4 as uuidV4 } from "uuid";
 import { DailyTask, WeekTask } from "../app.types";
 import {
   DAILY_TASKS_2025_01_06 as DAILY_TASKS,
-  WEEKLY_TASKS_2025_01_06_V2 as WEEKLY_TASKS,
+  WEEKLY_TASKS_2025_01_12 as WEEKLY_TASKS,
 } from "./weekly-planner-generator-form.data";
 import "./weekly-planner-generator-form.styles.css";
 
--- src/components/weekly-planner-generator-form.data.ts
+++ src/components/weekly-planner-generator-form.data.ts
@@ -141,3 +141,41 @@ export const WEEKLY_TASKS_2025_01_06_V2 = [
   { id: "555ad56b-a4ea-43f6-b2e1-2f4cd0dac0d5", name: "cleanup clothing drawer", points: 3 },
   { id: "6668e8e7-dead-4ebf-991f-504452dc706b", name: "download/save ring tree estimate", points: 2 },
 ];
+
+export const WEEKLY_TASKS_2025_01_12 = [
+  { id: "aa80f852-3a6b-4ba9-8d2d-eccbcf1be370", name: "HR paternity followup", points: 6 },
+  { id: "83bb0a1c-6780-4797-bfe0-3496565fe306", name: "jeep registration facts", points: 8 },
+
+  { id: "fe0ca6e3-dcd4-46cd-819b-ed1420ad9875", name: "master bath ceiling vent cutout", points: 3 },
+  { id: "de0ca6e3-dcd4-46c9-819b-ed1420ad9875", name: "master bath putty coat 1", points: 4 },
+
+  { id: "7da85d80-81eb-4105-a7ab-093f4f27e528", name: "laundry install temp shelves", points: 3 },
+  { id: "255ad56b-a4ea-43f6-b2e1-2f4cd0dac0d5", name: "keto recipe list (10)", points: 4 },
+
+  { id: "0b216b14-75ea-4c77-b3d6-d22b7200e567", name: "start watership down book (thru chap 16)", points: 3 },
+  { id: "d37d27b1-a360-4a30-bb3e-b4016763107e", name: "read lacrosse book thru chap 1", points: 6 },
+
+  { id: "1440ae67-af57-495d-88c6-52a2e1a4b6c5", name: "read code complete chapter 18", points: 5 },
+  { id: "171353e3-e5df-478e-901e-ff266fd9c963", name: "read lacrosse book thru chap 2", points: 4 },
+
+  { id: "4b903b3f-a2c1-4a4f-972a-1d6cd3578072", name: "schedule surveyor", points: 3 },
+  { id: "5440ae67-af57-495d-88c6-52a2e1a4b6c5", name: "yard dig up/bag moss", points: 5 },
+
+  { id: "f5c0db6c-90b5-4fe7-9c82-3a10977d5e63", name: "organize google-drive files", points: 4 },
+  { id: "6d4382ac-adf3-448a-b8f5-cbf0eefdc924", name: "organize file-base files", points: 6 },
+
+  { id: "c4b817e5-00e9-407d-9300-ceede081e92f", name: "list app static updates", points: 4 },
+  { id: "5722666d-076e-4c03-a244-2287643fcab2", name: "scheduler app mvp (planning & actual)", points: 6 },
+
+  { id: "bcc461cd-b791-4eec-99ae-47f440a1b8a8", name: "master bath putty coat 2", points: 4 },
+  { id: "57220edd-076e-4c03-a244-2287643fcab2", name: "master bath putty coat 3", points: 6 },
+
+  { id: "aaa461cd-b791-4eec-99ae-47f440a1b8a8", name: "reconnect fitbit", points: 4 },
+  { id: "bbb20edd-076e-4c03-a244-2287643fcab2", name: "monthly budget", points: 4 },
+
+  { id: "b55461cd-b791-4eec-99ae-47f440a1b8a8", name: "co2 detectors (garage/top-floor)", points: 2 },
+  { id: "f0f38b30-7ece-4452-a6ba-f497b69a8fb9", name: "pop-culture website brain session (2hr)", points: 4 },
+
+  { id: "220ad56b-a4ea-43f6-b2e1-2f4cd0dac0d5", name: "organize physical file", points: 5 },
+  { id: "3878e8e7-dead-4ebf-991f-504452dc706b", name: "jeep registration appt", points: 4 },
+];
