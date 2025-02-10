ca457d9a2bf288e4072817203ee9231234c114bd
Cam Dziurgot <camerondziurgot@gmail.com>
2025-02-09 22:39:18 -0500

    add feb 10/17 data

--- src/sections/week-planner/generator-form/generator-form.data.ts
+++ src/sections/week-planner/generator-form/generator-form.data.ts
@@ -375,3 +375,54 @@ export const WEEKLY_TASKS_2025_02_03 = [
   { id: "555461cd-b791-4eec-99ae-47f440a1b8a8", name: "house (kitchen) - cabinet lower shelf", points: 2 },
   { id: "de0ca6e3-dcd4-46c9-819b-ed1420ad9875", name: "finance - new car (SUV made USA) research", points: 4 },
 ];
+
+export const WEEKLY_TASKS_2025_02_10 = [
+  { id: "57220edd-076e-4c03-a244-2287643fcab2", name: "house (master bath) - flatten shower wall", points: 3 },
+  { id: "45454545-076e-4c03-a244-2287643fcab2", name: "house (master bath) - putty coat 3", points: 6 },
+
+  { id: "bcc461cd-b791-4eec-99ae-47f440a1b8a8", name: "house (master bath) - sand walls", points: 4 },
+  { id: "2aa461cd-b791-4eec-99ae-47f440a1b8a8", name: "house (master bath) - prime walls/ceiling", points: 5 },
+
+  { id: "4b903b3f-a2c1-4a4f-972a-1d6cd3578072", name: "house (yard) - schedule surveyor", points: 4 },
+  { id: "bbb20edd-076e-4c03-a244-2287643fcab2", name: "finance - monthly budget/paycheck distribution", points: 4 },
+
+  { id: "83bb0a1c-6780-4797-bfe0-3496565fe306", name: "finance - jeep registration facts", points: 8 },
+  { id: "555666cd-b791-4eec-99ae-47f440a1b8a8", name: "biz - pi home server session (2hr)", points: 6 },
+
+  { id: "5722666d-076e-4c03-a244-2287643fcab2", name: "biz - scheduler app data modeling session (2hr)", points: 4 },
+  { id: "12321231-076e-4c03-a244-2287643fcab2", name: "biz - scheduler app mvp (planning & actual)", points: 6 },
+
+  { id: "f0f38b30-7ece-4452-a6ba-f497b69a8fb9", name: "biz - pop-culture website brain session (2hr)", points: 4 },
+  { id: "6d4382ac-adf3-448a-b8f5-cbf0eefdc924", name: "organize - file-base files", points: 6 },
+
+  { id: "b55461cd-b791-4eec-99ae-47f440a1b8a8", name: "read - code complete chapter 22", points: 2 },
+  { id: "1440ae67-af57-495d-88c6-52a2e1a4b6c5", name: "read - lacrosse book thru chap 7", points: 3 },
+
+  { id: "88888888-3333-4eec-99ae-47f440a1b8a8", name: "read - lacrosse book thru chap 8", points: 4 },
+  { id: "1aa461cd-b791-4eec-99ae-47f440a1b8a8", name: "read - lacrosse book thru chap 9", points: 4 },
+
+  { id: "555461cd-b791-4eec-99ae-47f440a1b8a8", name: "house (kitchen) - cabinet lower shelf", points: 2 },
+  { id: "de0ca6e3-dcd4-46c9-819b-ed1420ad9875", name: "finance - new car (SUV made USA) research", points: 4 },
+];
+
+export const WEEKLY_TASKS_2025_02_17 = [
+  { id: "fe0ca6e3-dcd4-46cd-819b-ed1420ad9875", name: "house (master bath) - ceiling vent cutout", points: 3 },
+  { id: "aa80f852-3a6b-4ba9-8d2d-eccbcf1be370", name: "finance - paternity followup", points: 4 },
+
+  { id: "3878e8e7-dead-4ebf-991f-504452dc706b", name: "finance - jeep registration appt", points: 4 },
+  { id: "16734632-a2c1-4a4f-972a-1d6cd3578072", name: "biz - complete a11y course with certificate", points: 4 },
+
+  { id: "f0f38b30-7ece-4452-a6ba-f497b69a8fb9", name: "biz - pop-culture website brain session (2hr)", points: 4 },
+  { id: "12356432-7ece-4452-a6ba-f497b69a8fb9", name: "biz - video game session (2hr)", points: 4 },
+
+  { id: "f5c0db6c-90b5-4fe7-9c82-3a10977d5e63", name: "organize - google-drive files", points: 4 },
+
+  { id: "5440ae67-af57-495d-88c6-52a2e1a4b6c5", name: "house (yard) - trim west row tree limbs", points: 5 },
+  { id: "aaa461cd-b791-4eec-99ae-47f440a1b8a8", name: "house (yard) - trim north row tree limbs", points: 4 },
+
+  { id: "b55461cd-b791-4eec-99ae-47f440a1b8a8", name: "read - code complete chapter 23", points: 2 },
+  { id: "1440ae67-af57-495d-88c6-52a2e1a4b6c5", name: "read - lacrosse book thru chap 10", points: 3 },
+
+  { id: "88888888-3333-4eec-99ae-47f440a1b8a8", name: "read - lacrosse book thru chap 11", points: 4 },
+  { id: "1aa461cd-b791-4eec-99ae-47f440a1b8a8", name: "read - lacrosse book thru chap 12", points: 4 },
+];
